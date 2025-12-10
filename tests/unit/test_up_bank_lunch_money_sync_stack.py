0aea46def142433fd4bf7a0285c8bc23117ae4b9
Dylan Pinn <me@dylanpinn.com>
2025-12-11 00:10:50 +1100

    fix: reduce processor Lambda timeout and adjust SQS visibility timeout
    - Reduce processor Lambda timeout from 5 minutes to 2 minutes
    - Adjust SQS visibility timeout to 12 minutes (6x Lambda timeout)
    - Update CloudWatch alarm threshold to 96 seconds (80% of new timeout)
    - Fix and update stack tests with proper secret ARNs
    
    Fixes #10

--- tests/unit/test_up_bank_lunch_money_sync_stack.py
+++ tests/unit/test_up_bank_lunch_money_sync_stack.py
@@ -1,3 +1,5 @@
+import os
+
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
@@ -6,10 +8,16 @@ from up_bank_lunch_money_sync.up_bank_lunch_money_sync_stack import UpBankLunchM
 # example tests. To run these tests, uncomment this file along with the example
 # resource in up_bank_lunch_money_sync/up_bank_lunch_money_sync_stack.py
 def test_sqs_queue_created():
+    # Set valid ARNs for testing
+    os.environ["WEBHOOK_SECRET_ARN"] = "arn:aws:secretsmanager:us-east-1:123456789012:secret:test-webhook-abcdef"
+    os.environ["UP_API_KEY_ARN"] = "arn:aws:secretsmanager:us-east-1:123456789012:secret:test-up-api-abcdef"
+    os.environ["LUNCHMONEY_API_KEY_ARN"] = "arn:aws:secretsmanager:us-east-1:123456789012:secret:test-lunchmoney-abcdef"
+    
     app = core.App()
     stack = UpBankLunchMoneySyncStack(app, "up-bank-lunch-money-sync")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    # Test SQS queue has correct visibility timeout (12 minutes = 720 seconds)
+    template.has_resource_properties("AWS::SQS::Queue", {
+        "VisibilityTimeout": 720
+    })
